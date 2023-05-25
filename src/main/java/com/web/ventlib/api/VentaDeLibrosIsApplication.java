6983301102f9767f3aed846661e3242af6a02a0f
JorgeAlexisRM <alex.jruam@gmail.com>
2023-05-25 12:35:27 -0600

    Creando Repo

---
+++ src/main/java/com/web/ventlib/api/VentaDeLibrosIsApplication.java
@@ -0,0 +1,15 @@
+package com.web.ventlib.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@SpringBootApplication
+public class VentaDeLibrosIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaDeLibrosIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/ventlib/api/VentaDeLibrosIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.ventlib.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaDeLibrosIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
